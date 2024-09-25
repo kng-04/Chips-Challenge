ac3a7fb96d118998590be7eb81f48dc7074b9f2b
fbright <fletcbet1660@gmail.com>
2024-09-25 20:40:54 +1200

    Add main class

---
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/Main.java
@@ -0,0 +1,9 @@
+package nz.ac.wgtn.swen225.lc.app;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(Gui::new);
+    }
+}
