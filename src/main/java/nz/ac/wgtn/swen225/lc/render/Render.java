2097dc141bcd578ed3e60900cff4b5d8ad4febe5
Krystal Ng <krystal.ng@ecs.vuw.ac.nz>
2024-10-04 19:13:45 +1300

    added load game functionality

--- src/main/java/nz/ac/wgtn/swen225/lc/render/Render.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/render/Render.java
@@ -1,32 +1,36 @@
 package nz.ac.wgtn.swen225.lc.render;
 
 import nz.ac.wgtn.swen225.lc.domain.Game;
-import nz.ac.wgtn.swen225.lc.domain.Item;
+import nz.ac.wgtn.swen225.lc.domain.KeyTile;
 import nz.ac.wgtn.swen225.lc.domain.Tile;
+import nz.ac.wgtn.swen225.lc.domain.TreasureTile;
+import nz.ac.wgtn.swen225.lc.persistency.Persistency;
 
 import javax.sound.sampled.Clip;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 import java.util.List;
 
 public class Render extends JPanel {
     private Tile[][] board;
     private int x;
     private int y;
-    private List<Item> keys;
-    private List<Item> treasure;
+    private List<KeyTile> keys;
+    private List<TreasureTile> treasure;
     public Game game;
-    //public Load load;
     public Clip clip;
 
-    public Render(Game game) {
-        this.game = game;
-//        this.load = this.game.getLoad();
-        //this.keys = load.getKeys();
-//        this.treasure = load.getTreasures();
+    public Render(String gameFile) throws IOException {
+        // Load the game using the loadGame method from Persistency
+        this.game = Persistency.loadGame(gameFile);
+
+        // Initialize board dimensions
         this.x = game.getWidth();
         this.y = game.getHeight();
-//        this.board = load.getMap();
     }
+
+    public void paintComponent(Graphics g) {}
 
     public void playBackgroundMusic() {}
 
