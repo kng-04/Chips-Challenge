bb13f5af86b568cc813ca843e483cc157d3e93a0
fbright <fletcbet1660@gmail.com>
2024-10-17 21:48:24 +1300

    make the level timer use remaing time from the game state instead of the default 60

--- src/main/java/nz/ac/wgtn/swen225/lc/app/SaveManager.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/SaveManager.java
@@ -19,6 +19,7 @@ public class SaveManager {
     // saves using date and time instead of file picker
     public void autoSave(){
         gui.pauseGame();
+        Gui.game.setSecondsLeft(gui.levelTimer.getRemainingSeconds());
         LocalDateTime time = LocalDateTime.now();
         String fileName = time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
         fileName = "save_" + fileName + ".json";
