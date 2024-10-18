9715fe8d5670dbb87d638630ed952257f389d724
Krystal Ng <krystal.ng@ecs.vuw.ac.nz>
2024-10-19 00:53:03 +1300

    fix: info tile

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Chap.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Chap.java
@@ -51,10 +51,7 @@ public class Chap extends Characters{
                     firstMove = false;
                     game.setGame();
                 }
-                // Add FreeTile only if firstMove is true
-                game.addTile(new FreeTile(this.x - dx, this.y - dy));
             } else {
-
                 System.out.println("Chap cannot move onto that tile!");
             }
         }else{
